0bf120d1726831c5274153a462a8bf77fc22ad55
Z User <z@container>
2026-08-07 18:23:55 +0000

    revert: motorista nao vai no XML NF-e, era pro TudoEntregue

--- services/sieg-nfe-xml.js
+++ services/sieg-nfe-xml.js
@@ -541,25 +541,12 @@ ${xmlEndereco(partner, 'enderDest')}
   // === transp ===
   const modFrete = String(cfg.modFrete || '9');
   const hasVolume = cfg.qVol != null && Number(cfg.qVol) > 0;
-  const transporta = cfg.transporta || null;
-
-  let transportaBlock = '';
-  if (transporta && (transporta.xNome || transporta.CNPJ || transporta.CPF)) {
-    const docTransp = onlyNum(transporta.CNPJ || transporta.CPF || transporta.cnpj_cpf || '');
-    const docTag = docTransp.length === 14 ? 'CNPJ' : (docTransp.length === 11 ? 'CPF' : '');
-    transportaBlock = `
-      <transporta>${docTag ? `
-        <${docTag}>${docTransp}</${docTag}>` : ''}
-        <xNome>${esc(transporta.xNome || '')}</xNome>
-      </transporta>`;
-  }
-
   xml += `
     <transp>
       <modFrete>${modFrete}</modFrete>${hasVolume ? `
       <vol>
         <qVol>${Math.trunc(Number(cfg.qVol))}</qVol>
-      </vol>` : ''}${transportaBlock}
+      </vol>` : ''}
     </transp>`;
 
   // === cobr (duplicatas) ===
